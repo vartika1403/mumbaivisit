75cb99219a55fb96f752d36c3f7ad7ac5377ee6f
vartika1403 <vartika.1403@gmail.com>
2017-11-26 18:26:44 +0530

    new weather data done and displayed

--- app/src/main/java/mumbaivisit/myapplication/FragmentWeatherDisplay.java
+++ app/src/main/java/mumbaivisit/myapplication/FragmentWeatherDisplay.java
@@ -7,15 +7,17 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -26,8 +28,6 @@ import retrofit2.Response;
 public class FragmentWeatherDisplay extends Fragment {
     private static final String LOG_TAG = FragmentWeatherDisplay.class.getSimpleName();
     private final static String API_KEY = "9351aee12441dbae1f55fb5ac1de496b";
-    private JSONObject jsonObject;
-
     @BindView(R.id.city_name)
     TextView cityName;
     @BindView(R.id.city_temp)
@@ -36,6 +36,10 @@ public class FragmentWeatherDisplay extends Fragment {
     TextView cityHumidity;
     @BindView(R.id.city_pressure)
     TextView cityPressure;
+    @BindView(R.id.forecast_data)
+    LinearLayout forecastDataLayout;
+    private JSONObject jsonObject;
+    private ArrayList<WeatherResponse> weatherResponseList = new ArrayList<WeatherResponse>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -70,10 +74,6 @@ public class FragmentWeatherDisplay extends Fragment {
         Log.i(LOG_TAG, "call url, " + call.request().url().toString());
 
         call.enqueue(new Callback<JsonElement>() {
-          /*  @Override
-            public void onResponse(Response<JsonElement> response) {
-                Log.i(LOG_TAG, "response, " + response.body());
-            }*/
 
             @Override
             public void onResponse(Call<JsonElement> call, Response<JsonElement> response) {
@@ -83,24 +83,24 @@ public class FragmentWeatherDisplay extends Fragment {
                 JsonArray values = jsonElement.getAsJsonArray();
                 Log.i(LOG_TAG, "values, " + values);
                 Log.i(LOG_TAG, "values size, " + values.size());
-             // JsonElement jsonObject = jsonElement.getAsJsonArray().get("main");
                 for (int i = 0; i < values.size(); i++) {
                     JsonElement jsonObject = values.get(i);
                     Log.i(LOG_TAG, "jsonObject, " + jsonObject);
                     JsonElement main = jsonObject.getAsJsonObject().get("main");
                     Log.i(LOG_TAG, "main, " + main);
-
+                    Gson gson = new Gson();
+                    final WeatherResponse weatherResponse = gson.fromJson(main, WeatherResponse.class);
+                    Log.i(LOG_TAG, "weatherResponse, " + weatherResponse);
+                    Log.i(LOG_TAG, "temp, " + weatherResponse.getTemp());
+                    weatherResponseList.add(weatherResponse);
                 }
-                Gson gson = new Gson();
-                final WeatherResponse weatherResponse = gson.fromJson(jsonElement, WeatherResponse.class);
-              //  Log.i(LOG_TAG, )
-                Log.i(LOG_TAG, "temp, " + weatherResponse.getTemp());
+
                 if (getActivity() != null) {
                     (getActivity()).runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
                             try {
-                                updateViewWithData(weatherResponse);
+                                updateViewWithData(weatherResponseList);
                             } catch (JSONException e) {
                                 e.printStackTrace();
                             }
@@ -117,15 +117,26 @@ public class FragmentWeatherDisplay extends Fragment {
         });
     }
 
-    private void updateViewWithData(WeatherResponse weatherObject) throws JSONException {
+    private void updateViewWithData(ArrayList<WeatherResponse> weatherResponseList) throws JSONException {
         String cityNameText = "Mumbai";
-        cityName.setText(cityNameText);
+        cityTemp.setText(" " + weatherResponseList.get(0).getTemp());
+        cityPressure.setText(" " + weatherResponseList.get(0).getPressure());
+        cityHumidity.setText(" " + weatherResponseList.get(0).getHumidity());
+        for (WeatherResponse weatherResponse: weatherResponseList) {
+            View view = getActivity().getLayoutInflater().inflate(R.layout.forcast_child_data, null);
+            TextView childTempText = (TextView) view.findViewById(R.id.child_temp_text);
+            childTempText.setText(" " + weatherResponse.getTemp());
+            TextView childPressureText = (TextView) view.findViewById(R.id.child_pressure_text);
+            childPressureText.setText(" " + weatherResponse.getPressure());
+            forecastDataLayout.addView(view);
+        }
+        /*cityName.setText(cityNameText);
         double tempValue = weatherObject.getTemp();
         double pressureValue = weatherObject.getPressure();
         double humidityValue = weatherObject.getHumidity();
-        Log.i(LOG_TAG, "tempValue, " +tempValue);
+        Log.i(LOG_TAG, "tempValue, " + tempValue);
         cityTemp.setText("TEMPERATURE: " + tempValue);
         cityPressure.setText("PRESSURE: " + pressureValue);
-        cityHumidity.setText("HUMIDITY: " + humidityValue);
+        cityHumidity.setText("HUMIDITY: " + humidityValue);*/
     }
 }
